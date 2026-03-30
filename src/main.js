08bd6626d3de325075727f7de01dca9a78bf05ab
Jacqueline <jack@Jacquelines-MacBook-Air.local>
2026-03-30 11:04:16 -0400

    added remove to import, updated delete Btn block, updated css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './style.css';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue, set, push } from 'firebase/database';
+import { getDatabase, ref, onValue, set, push, remove } from 'firebase/database';
 import confetti from 'canvas-confetti';
 
 //  FIREBASE CONFIGURATION
@@ -131,11 +131,11 @@ onValue(reportsRef, (snapshot) => {
                 el.style.backgroundColor = 'transparent';
             } else {
                 el.classList.add('offer');
-                el.style.backgroundColor = report.color;
-                el.style.borderRadius = '50%';
-                el.style.width = '20px';
-                el.style.height = '20px';
-                el.style.border = '2px solid white';
+                 el.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="${report.color}" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="${report.color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
+    `;
+                el.style.backgroundColor = 'transparent';
             }
 
             const popup = new mapboxgl.Popup({ offset: 25, anchor: 'bottom' })
