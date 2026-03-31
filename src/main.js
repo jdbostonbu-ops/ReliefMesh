19442d043d72bfeabcd4b751b813137ca4635947
Jacqueline <jack@Jacquelines-MacBook-Air.local>
2026-03-31 12:25:18 -0400

    add currentPostType to const newReport, updated svg for need, updated js, removed notes files and removed note.js file

--- src/main.js
+++ src/main.js
@@ -109,7 +109,7 @@ onValue(reportsRef, (snapshot) => {
             map.getSource('reports-source').setData({ 'type': 'FeatureCollection', 'features': features });
         }
 
-       Object.keys(data).forEach(key => {
+    Object.keys(data).forEach(key => {
     const report = data[key];
 
     const myPosts = JSON.parse(localStorage.getItem('my_posts') || "[]");
@@ -137,13 +137,18 @@ onValue(reportsRef, (snapshot) => {
     }); // FIXED: was )}
 
     // --- FORMING TRIANGLES / CIRCLES ---
-    if (report.type === 'need') {
+    
+    el.className = 'sos-marker pulse';
+
+if (report.type === 'need') {
+   
         el.classList.add('need');
         el.innerHTML = `
-            <svg xmlns="http://www.w3.org" fill="${report.color}33" viewBox="0 0 24 24" stroke-width="1.5" stroke="${report.color}" style="width: 32px; height: 32px;">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="${report.color}" fill-opacity="0.2" stroke="${report.color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
             </svg>`;
-        el.style.backgroundColor = 'transparent';
+         el.style.backgroundColor = 'transparent';
+        
     } else {
         el.classList.add('offer');
         el.innerHTML = `
@@ -430,7 +435,8 @@ postNeedBtn.addEventListener('click', () => {
               color: color,
               item: item,
               msg: `Urgent ${category} request: ${item}`,
-              loc: [lng, lat]
+              loc: [lng, lat],
+              type: currentPostType,
           };
         // Push to Firebase
         const newReportRef = push(ref(db, 'reports'));
