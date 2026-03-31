89df88f670eef7ea3c9a531cd77528a154401f64
Jacqueline <jack@Jacquelines-MacBook-Air.local>
2026-03-30 22:16:25 -0400

    updated css and js

--- src/main.js
+++ src/main.js
@@ -111,7 +111,7 @@ onValue(reportsRef, (snapshot) => {
 
         Object.keys(data).forEach(key => {
             const report = data[key];
-/***************************************UNSUCCESSFULE FORMING "TRIANGLES"/ ONLY CIRCLE MARKERS ***************************************/
+/*************************************** FORMING "TRIANGLES"/ ONLY CIRCLE MARKERS ***************************************/
 const el = document.createElement('div');
 el.className = 'sos-marker pulse';
 
@@ -122,7 +122,7 @@ el.className = 'sos-marker pulse';
 
 if (report.type === 'need') {
     el.classList.add('need');
-    // Using your Heroicon SVG with dynamic colors
+    
     el.innerHTML = `
         <svg xmlns="http://www.w3.org" fill="${report.color}33" viewBox="0 0 24 24" stroke-width="1.5" stroke="${report.color}" style="width: 32px; height: 32px;">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
